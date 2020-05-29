dcb4f446e2f8ffbf40008c77f557cf5c91469a11
sandrasungura <63394467+sandrasungura@users.noreply.github.com>
2020-05-29 18:16:51 +0200

    Update jquery.js

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -8,10 +8,9 @@ $("#submit").click(function() {
 var name = $("#name").val();
 var email = $("#email").val();
 var message = $("#message").val();
-var contact = $("#contact").val();
 $("#returnmessage").empty(); // To empty previous error/success message.
 // Checking for blank fields.
-if (name == '' || email == '' || contact == '') {
+if (name == '' || email == '') {
 alert("Please Fill Required Fields");
 } else {
 // Returns successful data submission message when the entered information is stored in database.
@@ -19,7 +18,6 @@ $.post("contact_form.php", {
 name1: name,
 email1: email,
 message1: message,
-contact1: contact
 }, function(data) {
 $("#returnmessage").append(data); // Append returned message to message paragraph.
 if (data == "Your Query has been received, We will contact you soon.") {
