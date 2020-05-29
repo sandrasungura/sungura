de3c9b0f8a423a23443b2242655b72d672fb7309
sandrasungura <63394467+sandrasungura@users.noreply.github.com>
2020-05-29 10:50:14 +0200

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -3,10 +3,8 @@ function sendEmail() {
 	Email.send({
 	Host: "smtp.gmail.com",
 	Username : "<sender’s email address>",
-	Password : "<email password>",
 	To : '<sandra.m.revilla@gmail.com>',
 	From : "<sender’s email address>",
-	Subject : "<email subject>",
 	Body : "<email body>",
 	}).then(
 		message => alert("mail sent successfully")
