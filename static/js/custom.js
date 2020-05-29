07f10afef58aaae06b702678decf233b66ad9455
sandrasungura <63394467+sandrasungura@users.noreply.github.com>
2020-05-29 18:04:02 +0200

    Update custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -8,14 +8,4 @@
     $('.preloader').fadeOut(1000); // set duration in brackets    
   });
 
-function sendEmail() {
-	Email.send({
-	Host: "smtp.gmail.com",
-	Username : "<sender’s email address>",
-	To : '<sandra.m.revilla@gmail.com>',
-	From : "<sender’s email address>",
-	Body : "<email body>",
-	}).then(
-		message => alert("mail sent successfully")
-	);
-}
+
