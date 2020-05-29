73724a6e040b4c961075786d9bd94b6ff55440cc
sandrasungura <63394467+sandrasungura@users.noreply.github.com>
2020-05-29 19:59:52 +0200

    Update ContactForm.php

--- static/js/ContactForm.php
+++ static/js/ContactForm.php
@@ -4,7 +4,7 @@
 	$message = $_POST [‘message’];
 
 
-$email_from = "$visitor_email";
+$email_from = 'al264048@uji.es';
 $email_subject = “New contact form”;
 $email_body =  “User Name: $name. \n”.
 		“User Email: \n”.
@@ -14,5 +14,5 @@ $email_body =  “User Name: $name. \n”.
 	$headers =  “From: $email_from \r\n”;
 	$headers =  “Reply-To: $visitor_email \r\n”;
 	mail($to, $email_subject, $email_body, $headers);
-	header (“Location: contact.md”);
+	header (“Location: sungura/content/top/contact.md”);
 ?>
